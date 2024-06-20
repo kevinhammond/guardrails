df48e84147fc209b56141fba14e6dac28d62f2d8
Kevin Hammond <12563287+kevinhammond@users.noreply.github.com>
2024-06-20 22:15:57 +0100

    copied constitution-priv

---
+++ src/Cardano/Constitution/Config/Instance/TxLift.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+module Cardano.Constitution.Config.Instance.TxLift () where
+
+import PlutusCore.Default as Tx (DefaultUni)
+import PlutusTx.Lift as Tx
+import PlutusTx.Lift.Class as Tx
+import Cardano.Constitution.Config.Types
+import Language.Haskell.TH as TH
+
+-- `Tx.makeLift` depends on TH which is sensitive to re-ordering; try to NOT reorder the following.
+----------
+
+Tx.makeLift ''PredKey
+
+deriving newtype instance (Tx.Typeable Tx.DefaultUni predValue, Tx.Lift Tx.DefaultUni predValue)
+                          => Tx.Lift Tx.DefaultUni (Predicates predValue)
+
+Tx.makeTypeable (TH.ConT ''Tx.DefaultUni) ''Predicates
+Tx.makeLift ''ParamValue
+Tx.makeLift ''ConstitutionConfig
